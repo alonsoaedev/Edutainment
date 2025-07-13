96d33dfe1ba5e1d78c8ecda623451784843aff29
Alonso Acosta <alonso.ae@hotmail.com>
2025-07-12 21:18:52 -0600

    Build the settings view

--- Edutainment/ContentView.swift
+++ Edutainment/ContentView.swift
@@ -8,12 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    let levels = ["easy", "medium", "hard"]
+    
+    @State private var tableNumber = 2
+    @State private var questionsNumber = 1
+    @State private var selectedLevel = "easy"
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            
+            Stepper("Table number: \(tableNumber)", value: $tableNumber, in: 2...12, step: 1)
+            Stepper("Number of questions: \(questionsNumber)", value: $questionsNumber, in: 1...10)
+            Picker("Level", selection: $selectedLevel) {
+                ForEach(levels, id: \.self) { level in
+                    Text("\(level)")
+                }
+            }
+            .pickerStyle(.segmented)
+            
+            Spacer()
+            
+            Button("Start") {}
         }
         .padding()
     }
