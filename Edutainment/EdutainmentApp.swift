3c0afb8ea136e2b5492093fd454f5f81ab3a508f
Alonso Acosta <alonso.ae@hotmail.com>
2025-07-12 20:31:52 -0600

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Alonso Acosta Enriquez on 12/07/25.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
