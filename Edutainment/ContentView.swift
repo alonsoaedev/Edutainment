249c6a3b4dbf1ea96fcee8d229a80225e30e23da
Alonso Acosta <alonso.ae@hotmail.com>
2025-07-13 12:50:29 -0600

    Generate questions given the selected settings

--- Edutainment/ContentView.swift
+++ Edutainment/ContentView.swift
@@ -7,12 +7,76 @@
 
 import SwiftUI
 
+struct MultiplicationQuestion {
+    private let questionTemplates: [String: String] = [
+        "easy": "%d x %d = ?",
+        "medium": "%d x ? = %d",
+        "hard": "? x ? = %d",
+    ]
+
+    let level: String
+    let multiplier: Int = Int.random(in: 1...12)
+    let multiplicand: Int = Int.random(in: 1...12)
+    var product: Int { multiplier * multiplicand }
+    
+    var question: String {
+        if level == "hard" {
+            return "? x ? = \(product)"
+        }
+        
+        if level == "medium" {
+            return "\(multiplier) x ? = \(product)"
+        }
+        
+        return "\(multiplier) x \(multiplicand) = ?"
+    }
+    
+    var answer: String {
+        if level == "hard" {
+            return "\(multiplier),\(multiplicand)"
+        }
+        
+        if level == "medium" {
+            return "\(multiplicand)"
+        }
+        
+        return "\(product)"
+    }
+    
+    var options: [String] {
+        if level == "hard" {
+            return [
+                answer,
+                "\(Int.random(in: 1...12)), \(Int.random(in: 1...12))",
+                "\(Int.random(in: 1...12)), \(Int.random(in: 1...12))",
+            ].shuffled()
+        }
+        
+        return [
+            answer,
+            "\(Int.random(in: 1...12))",
+            "\(Int.random(in: 1...12))",
+        ].shuffled()
+    }
+}
+
 struct ContentView: View {
     let levels = ["easy", "medium", "hard"]
     
     @State private var tableNumber = 2
     @State private var questionsNumber = 1
     @State private var selectedLevel = "easy"
+    @State private var questions: [MultiplicationQuestion] = []
+    
+    func createQuestions(_ tableNumber: Int, _ questionsNumber: Int, _ level: String) -> [MultiplicationQuestion] {
+        var createdQuestions: [MultiplicationQuestion] = []
+        for _ in 1...questionsNumber {
+            createdQuestions.append(
+                MultiplicationQuestion(level: level)
+            )
+        }
+        return createdQuestions
+    }
     
     var body: some View {
         VStack {
@@ -29,7 +93,9 @@ struct ContentView: View {
             
             Spacer()
             
-            Button("Start") {}
+            Button("Start") {
+                questions = createQuestions(tableNumber, questionsNumber, selectedLevel)
+            }
         }
         .padding()
     }
