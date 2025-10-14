3108bdbbf17fce4058c594da8c7f0f8f6e564d04
Alonso Acosta <alonso.ae@hotmail.com>
2025-10-13 22:05:03 -0600

    Track the score of the game

--- Edutainment/views/Game.swift
+++ Edutainment/views/Game.swift
@@ -12,29 +12,55 @@ struct Game: View {
     
     @State private var currentQuestionIndex: Int = 0
     @State private var correctAnswer: Bool = false
+    @State private var score: Int = 0
+    @State private var isGameOver: Bool = false
     
     var body: some View {
         VStack {
-            Section("Question") {
-                Text("\(questions[currentQuestionIndex].question)")
-            }
-            
-            Section ("Options") {
-                ForEach(questions[currentQuestionIndex].options, id: \.self) { option in
-                    Button {
-                        correctAnswer = questions[currentQuestionIndex].isCorrect(answer: option)
-                        if currentQuestionIndex < questions.count - 1 {
-                            currentQuestionIndex += 1
-                        }
-                    } label: {
-                        Text(option)
-                            .frame(maxWidth: .infinity)
-                    }
-                    .buttonStyle(.borderedProminent)
+            if !isGameOver {
+                Section("Score") {
+                    Text("\(score)")
                 }
+                
+                Section("Question") {
+                    Text("\(questions[currentQuestionIndex].question)")
+                }
+                
+                Section ("Options") {
+                    ForEach(questions[currentQuestionIndex].options, id: \.self) { option in
+                        Button {
+                            correctAnswer = questions[currentQuestionIndex].isCorrect(answer: option)
+                            if currentQuestionIndex < questions.count - 1 {
+                                currentQuestionIndex += 1
+                            }
+                            if correctAnswer {
+                                score += 1
+                            }
+                            if currentQuestionIndex == questions.count - 1 {
+                                isGameOver = true
+                            }
+                        } label: {
+                            Text(option)
+                                .frame(maxWidth: .infinity)
+                        }
+                        .buttonStyle(.borderedProminent)
+                    }
+                }
+            } else {
+                Text("Your final score is \(score)")
             }
         }
         .navigationTitle("Game")
         .padding()
     }
+}
+
+#Preview {
+    Game(questions: [
+        MultiplicationQuestion(level: "easy", multiplier: 2),
+        MultiplicationQuestion(level: "easy", multiplier: 2),
+        MultiplicationQuestion(level: "easy", multiplier: 2),
+        MultiplicationQuestion(level: "easy", multiplier: 2),
+        MultiplicationQuestion(level: "easy", multiplier: 2),
+    ])
 }
